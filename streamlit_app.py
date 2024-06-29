5248d26444c21fd3b7ad5a0a35779ff8b3f8eb67
Boreadi <bmmahlakwane@gmail.com>
2024-06-29 21:40:18 +0200

    Button success

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,17 @@
 import streamlit as st
+#from st_audiorec import st_audiorec
 
 
-st.write("Boreadi")
+
+st.title("Vula Nami")
+
+
+st.button('Start recording')
+st.button('Stop')
+st.button('Reset')
+
+
+#av_audio_data = st_audiorec()
+
+#if wav_audio_data is not None:
+    #st.audio(wav_audio_data, format='audio/wav')
